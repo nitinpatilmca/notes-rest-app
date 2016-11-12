70923a606621ef4955098460f3b1db01d5c8644d
Nitin Patil <nitinpatil.mca@gmail.com>
2016-11-12 23:58:50 +0530

    Configure jsp view location and Jackson converter mapping added

--- Notes/src/org/gotprint/config/WebConfig.java
+++ Notes/src/org/gotprint/config/WebConfig.java
@@ -1,6 +1,56 @@
 package org.gotprint.config;
 
-public class WebConfig  {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+
+@Configuration
+@EnableWebMvc
+@EnableScheduling
+@ComponentScan("org.gotprint")
+@Import({ SecurityConfig.class })
+public class WebConfig extends WebMvcConfigurerAdapter {
+		
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		return multipartResolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	
+	@Bean
+	public Jackson2ObjectMapperBuilder jacksonBuilder() {
+		Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
+		return b;
+	}
+	
 
 }
 
