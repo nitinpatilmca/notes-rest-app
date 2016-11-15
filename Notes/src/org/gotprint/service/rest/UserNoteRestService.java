888eeec647a1806cf70e9e6d8bd09b7350727639
Nitin Patil <nitinpatil.mca@gmail.com>
2016-11-15 13:17:42 +0530

    return user notes function added, rest call modified to userNotes

--- Notes/src/org/gotprint/service/rest/UserNoteRestService.java
+++ Notes/src/org/gotprint/service/rest/UserNoteRestService.java
@@ -8,12 +8,13 @@ import org.gotprint.domain.Notes;
 import org.gotprint.domain.Users;
 import org.gotprint.exceptions.NotesAppBaseException;
 import org.gotprint.exceptions.UserException;
+import org.gotprint.model.NotesJson;
 import org.gotprint.model.NotesWrapper;
 import org.gotprint.service.NoteService;
 import org.gotprint.service.UserService;
+import org.gotprint.utils.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,9 @@ public class UserNoteRestService {
 	
 	@Autowired
 	private UserService userService;	
+
+	@Autowired
+	private Utility utility;
 	
 	//------------------Add new user -------------------------------------------------------------------------
 	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
@@ -61,14 +65,14 @@ public class UserNoteRestService {
 	}
 	
 	//------------------Retrieve Notes list for user id -------------------------------------------------------------
-	@RequestMapping(value = "/listNote/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Notes>>  retrieveUserNotes(@PathVariable("userId") String userId) throws NotesAppBaseException{		
-		//List<Notes> listOfNotes = userNoteService.getNotes(user);
-		return new ResponseEntity<List<Notes>>(userNoteService.getNotes(userId), HttpStatus.OK);
-		
+	@RequestMapping(value = "/userNotes/{userId}", method = RequestMethod.GET)
+	public ResponseEntity<List<NotesJson>>  retrieveUserNotes(@PathVariable("userId") String userId) throws NotesAppBaseException{	
+		List<Notes>  sorucelist = userNoteService.getNotes(userId);
+		List<NotesJson> listOfNotes =  utility.copyNotes(sorucelist);
+		return new ResponseEntity<List<NotesJson>>(listOfNotes, HttpStatus.OK);
 	}
 	
-
+	//------------------Handle exception at this class -------------------------------------------------------------
 	@ExceptionHandler(NotesAppBaseException.class)
 	public ResponseEntity<String> rulesForUserNotes(HttpServletRequest request, Exception e)  {
 		return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
