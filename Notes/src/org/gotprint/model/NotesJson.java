26be979ed56f528775ca3da655c64151eb446084
Nitin Patil <nitinpatil.mca@gmail.com>
2016-11-15 13:19:37 +0530

    Represent the user notes data to for display purpose

---
+++ Notes/src/org/gotprint/model/NotesJson.java
@@ -0,0 +1,30 @@
+package org.gotprint.model;
+
+
+public class NotesJson {
+	
+	private String title;
+	private String note;
+	private String lastUpdateOn;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getLastUpdateOn() {
+		return lastUpdateOn;
+	}
+	public void setLastUpdateOn(String lastUpdateOn) {
+		this.lastUpdateOn = lastUpdateOn;
+	}
+	
+	
+}
